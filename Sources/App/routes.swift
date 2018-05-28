0a600496c1a09a7f86ca76940bfbe7509a86a2ca
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-05-28 12:51:16 -0700

    uppercase the next paramter

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,13 +3,19 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+//    router.get("hello") { req in
+//        return "Hello, world!"
+//    }
+    
+    router.get("upcase", String.parameter) { req in
+        return try req.parameters.next(String.self).uppercased()
     }
-
+    
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
 }
+
+
