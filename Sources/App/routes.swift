d1ed26d76126161878bd0049880d04c417f068cc
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-05-29 12:39:47 -0700

    implemented a database

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,12 @@
 import Vapor
 
+struct Data: Content {
+    var name: String?
+    var value: String?
+}
+
+var database = [Data]()
+
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
@@ -9,6 +16,29 @@ public func routes(_ router: Router) throws {
     
     router.get("upcase", String.parameter) { req in
         return try req.parameters.next(String.self).uppercased()
+    }
+    
+    router.post("store") { req -> Future<HTTPStatus> in
+        return try req.content.decode(Data.self).map(to: HTTPStatus.self) { data in
+            database.append(data)
+            return .ok
+        }
+    }
+    
+    router.get("lookup", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        var value: String?
+        for data in database {
+            if name == data.name {
+                value = data.value
+            }
+        }
+        guard value != nil else { return "No value" }
+        return value!
+    }
+    
+    router.get("retrieve") { req in
+        return database
     }
     
     // Example of configuring a controller
